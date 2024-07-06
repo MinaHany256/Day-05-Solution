cfcef426713dc99249befac923feabf3e872625b
MinaHany256 <minahanyfoad@gmail.com>
2024-07-06 12:32:55 +0300

    Part 04 Enum - Example 01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -3,17 +3,58 @@ using Common;
 
 namespace Demo
 {
+    class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Gender Gender { get; set; }
+
+    }
+
+    enum Grade
+    {
+        // labels
+        A, B, C, D, E, F
+    }
+    enum Gender
+    {
+        Male,
+        Female
+    }
+
+    enum Branch : byte
+    {
+        Dokki = 202, NasrCity, Maadi = 252, Alex, SmartVillage, BNS
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            TypeA obj = new TypeA();
+            #region Access Modefiers
+            //TypeA obj = new TypeA();
 
-            //obj.x = 10; // invalid due to protection level (x is private)
+            ////obj.x = 10; // invalid due to protection level (x is private)
 
-            //obj.y = 10; // invalid due to protection level (x is internal)
+            ////obj.y = 10; // invalid due to protection level (x is internal)
 
-            obj.z = 1;   // valid
+            //obj.z = 1;   // valid 
+            #endregion
+
+            #region Example 01
+            //Person person = new Person();
+            //person.Gender = Gender.Male;
+
+            //Gender X = Gender.Male;
+
+            ///Grade grade = Grade.A;
+            ///if(grade == Grade.A)
+            ///    Console.WriteLine(":)");
+            ///else
+            ///    Console.WriteLine(":("); 
+            #endregion
+
+
 
         }
     }
