6a733ec5e7e68b100c9e776f2fc3d92778ae8ee3
MinaHany256 <minahanyfoad@gmail.com>
2024-07-07 22:10:47 +0300

    Session Task

---
+++ Task/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    enum Gender
+    {
+        Male,
+        Female
+    }
+    internal struct Employee
+    {
+
+        #region Attributes
+        private int id;
+        private string name;
+        private decimal salary;
+        private Gender gender;
+        #endregion
+
+        #region Constructor
+
+        public Employee (int Id, string Name, decimal Salary, Gender Gender)
+        {
+            this.id = Id;
+            this.name = Name;   
+            this.salary = Salary;
+            this.gender = Gender;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $" Id: {id} \n Name: {name} \n Salary: {salary} \n Gender: {gender}";
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get { return id; }      
+            set { id = value; }
+        }
+        public string Name
+        { get { return name; } set { name = value; } }
+
+        public decimal Salary
+            { get { return salary; } set { salary = value; } }
+
+        public Gender Gender { get { return gender; } set { gender = value; } }
+
+        #endregion
+
+    }
+}
--- Task/Program.cs
+++ Task/Program.cs
@@ -2,9 +2,46 @@
 {
     internal class Program
     {
+        public static void PrintEmployees()
+        {
+            int Id;
+            string Name;
+            decimal Salary;
+            Gender gender;
+
+            do
+            {
+                Console.Write("Enter Your Id: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out Id));
+
+
+
+            Console.Write("Enter Your Name: ");
+            Name = Console.ReadLine() ?? "NoName";
+
+
+            do
+            {
+                Console.Write("Enter Your Salary: ");
+            }
+            while (!decimal.TryParse(Console.ReadLine(), out Salary));
+
+            do
+            {
+                Console.Write("Enter Your Gender: ");
+            }
+            while (!Enum.TryParse<Gender>(Console.ReadLine(), true, out gender));
+
+            Employee employee = new Employee(Id, Name, Salary, gender);
+
+            Console.WriteLine(employee);
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Task");
+            PrintEmployees();
         }
     }
 }
