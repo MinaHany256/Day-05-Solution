a9ba4058bbb81151db8c8eaa08af2ec1a05f5cf8
MinaHany256 <minahanyfoad@gmail.com>
2024-07-07 23:44:02 +0300

    Question 03

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -15,7 +15,17 @@
         //} 
         #endregion
 
+        #region 3.Create an enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
 
+        //enum Season
+        //{
+        //    spring,
+        //    summer,
+        //    autumn,
+        //    winter
+        //}
+
+        #endregion
 
 
         static void Main(string[] args)
@@ -43,6 +53,39 @@
             //for (int i = 0; i < person.Size; i++)
             //{
             //    Console.WriteLine(person[i]);
+            //}
+
+            #endregion
+
+            #region 3.Create an enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+
+            //Console.Write("Please Enter a season: ");
+            
+
+            //if(Enum.TryParse(Console.ReadLine(),true, out Season season))
+            //{
+            //    switch(season)
+            //    {
+            //        case Season.spring:
+            //            Console.WriteLine("Spring is From March to May");
+            //            break;
+            //        case Season.summer:
+            //            Console.WriteLine("Summer is From June to August");
+            //            break;
+            //        case Season.autumn:
+            //            Console.WriteLine("Autumn is From September to November ");
+            //            break;
+            //        case Season.winter:
+            //            Console.WriteLine("Winter is From December to February");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid Input");
+            //            break;
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input");
             //}
 
             #endregion
